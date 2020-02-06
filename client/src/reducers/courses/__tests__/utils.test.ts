e5610d231148ca1f7d0a87e93726618eda59d16c
vycheslav popov <slavaavals616@gmail.com>
2020-02-07 00:28:07 +0400

    add a few tests of the reducer utils

--- client/src/reducers/courses/__tests__/utils.test.ts
+++ client/src/reducers/courses/__tests__/utils.test.ts
@@ -1,4 +1,4 @@
-import { rejectCourseById } from '../utils';
+import { rejectCourseById, changeCourseStatus, changeCourseTitle } from '../utils';
 import { Course, CourseStatus } from '../../../components/Courses/Courses.entities';
 
 describe('rejectCourseById', () => {
@@ -12,4 +12,27 @@ describe('rejectCourseById', () => {
     const expected: Course[] = [{ id: '2', title: 't2', description: 'd2', status: CourseStatus.Open }];
     expect(actual).toEqual(expected)
   });
-})
+});
+
+describe('changeCourseStatus', () => {
+  it('should change status of the received course', () => {
+    const course: Course = { id: '1', title: 't1', description: 'd1', status: CourseStatus.Open };
+
+    const actual: Course[] = changeCourseStatus([course], '1', CourseStatus.Progress);
+    const expected: Course[] = [{ id: '1', title: 't1', description: 'd1', status: CourseStatus.Progress }];
+
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe('changeCourseTitle', () => {
+  it('should change title of the received course', () => {
+    const course: Course = { id: '1', title: 't1', description: 'd1', status: CourseStatus.Open };
+    const newTitle = 'updated title';
+
+    const actual: Course[] = changeCourseTitle([course], '1', newTitle);
+    const expected: Course[] = [{ id: '1', title: newTitle, description: 'd1', status: CourseStatus.Open }];
+
+    expect(actual).toEqual(expected);
+  });
+});
