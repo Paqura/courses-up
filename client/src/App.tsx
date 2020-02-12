fee21588da31714967711ca6b53e63f954aec679
Vycheslav Popov <slavaavals616@gmail.com>
2020-02-12 19:03:24 +0400

    add graphql + apollo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,11 +3,20 @@ import { Courses } from './components/Courses';
 import { Provider } from 'react-redux';
 import { store } from './redux/configureStore';
 
+import { ApolloProvider } from 'react-apollo';
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({
+  uri: 'https://api-euwest.graphcms.com/v1/ck6jaw7u4jjga01d7cwgcaqhs/master',
+});
+
 const App: React.FC = () => {
   return (
-    <Provider store={store}>
-      <Courses />
-    </Provider>
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <Courses />
+      </Provider>
+    </ApolloProvider>
   );
 };
 
