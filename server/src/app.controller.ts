1070d190d7de8bbe28de98130455efbdd6058c3f
Vycheslav Popov <slavaavals616@gmail.com>
2020-03-03 21:16:58 +0400

    fake register: step1

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -2,16 +2,37 @@
 import { Controller, Request, Post, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 
+interface User {
+  name: string;
+  password: string;
+}
+
+const fakeDb: User[] = [
+  { name: 'slava', password: '123' },
+];
+
+const connectToDb = (): Promise<User[]> => new Promise(res => {
+  res(fakeDb);
+});
+
 @Controller()
 export class AppController {
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
-    return req.user;
+    return req.body;
   }
 
   @Post('auth/register')
   async register(@Request() req) {
+    const db = await connectToDb();
+
+    // check
+    if (db.find(user => user.name === req.body.name)) {
+      return 'User already exists';
+    }
+
+    // save
     return req.body;
   }
 }
