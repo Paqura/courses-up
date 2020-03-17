7abcb16b76d40e558878878ffe94ded83307d37d
vycheslav popov <slavaavals616@gmail.com>
2020-03-17 21:13:23 +0400

    remove commented code

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -4,17 +4,4 @@ import { UsersService } from '../users/users.service';
 @Injectable()
 export class AuthService {
   constructor(private readonly usersService: UsersService) {}
-
-  // async validateUser(username: string, pass: string): Promise<any> {
-  //   const user = await this.usersService.findOneByName(username);
-
-  //   if (user && user.password === pass) {
-  //     const { password, ...result } = user;
-  //     return result;
-  //   }
-
-  //   return null;
-  // }
-
-  // async createUser() {}
 }
