fffe8336bb5995ca5fe42bd587d2d86cfa6d36d4
vycheslav popov <slavaavals616@gmail.com>
2020-03-18 22:54:44 +0400

    add tests

--- server/test/app.e2e-spec.ts
+++ server/test/app.e2e-spec.ts
@@ -1,23 +1,69 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { CreateUserDto, LoginUserDto } from 'src/dto';
+import { HttpStatus } from '@nestjs/common';
+import * as mongoose from 'mongoose';
+import { DB_ENDPOINT } from '../src/config';
 
-describe('AppController (e2e)', () => {
-  let app;
+beforeAll(async () => {
+  await mongoose.connect(DB_ENDPOINT);
+  await mongoose.connection.db.dropDatabase();
+});
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+afterAll(done => {
+  mongoose.disconnect(done);
+});
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
+const app = 'http://localhost:8001';
+
+describe('AUTH', () => {
+
+  it('should return 401 status', () => {
+    return request(app)
+      .get('/auth')
+      .expect(HttpStatus.UNAUTHORIZED);
+  });
+});
+
+describe('AUTH', () => {
+  it('should register', () => {
+    const user: CreateUserDto = {
+      name: 'test1',
+      password: '1234',
+    };
+
+    return request(app)
+      .post('/auth/register')
+      .set('Accept', 'application/json')
+      .send(user)
+      .expect(({ body }) => {
+        expect(body.user.name).toEqual('test1');
+        expect(body.token).toBeDefined();
+      })
+      .expect(HttpStatus.CREATED);
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('should reject duplicate registration', () => {
+    const user: CreateUserDto = {
+      name: 'test1',
+      password: '1234',
+    };
+
+    return request(app)
+      .post('/auth/register')
+      .set('Accept', 'application/json')
+      .send(user)
+      .expect(HttpStatus.BAD_REQUEST);
+  });
+
+  it('should login', () => {
+    const user: LoginUserDto = {
+      name: 'test1',
+      password: '1234',
+    };
+
+    return request(app)
+      .post('/auth/login')
+      .send(user)
+      .expect(HttpStatus.CREATED);
   });
 });
