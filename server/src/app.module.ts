a929858012e8e86af5df1478cec7a74e3860cf7c
Vycheslav Popov <slavaavals616@gmail.com>
2020-03-18 11:28:39 +0400

    remove redundant module from imports

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,13 +3,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
 import { DB_ENDPOINT } from './config';
 
 @Module({
   imports: [
     AuthModule,
-    UsersModule,
     MongooseModule.forRoot(DB_ENDPOINT),
   ],
   controllers: [AppController],
